e80311120f567d2067211f41a91ee20e1148251e
Luca Béla Palkovics <luca.bela.palkovics@gmail.com>
2013-04-07 18:20:18 +0200

    Decode yuv4mpeg stream and render it

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include <framework.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
 
 class window: public koku::opengl::windowCallback
 {
 	private:
 		koku::opengl::window  my_window;
 		koku::opengl::buffer  my_rect;
-		koku::opengl::texture my_eye_left;
-		koku::opengl::texture my_eye_right;
+		koku::opengl::texture my_frame;
 		koku::opengl::shader  my_shader;  //will take left/right and render it right
-		koku::opengl::shader_uniform my_eye_sampler_left;
-		koku::opengl::shader_uniform my_eye_sampler_right;
+		koku::opengl::shader_uniform my_frame_sampler;
 		bool run;
+		int frame_w;
+		int frame_h;
+		unsigned char *frame_data;
+		unsigned char *frame_data_unpacked;
 
 	protected:
 		void onQuit()
@@ -22,12 +28,14 @@ class window: public koku::opengl::windowCallback
 		window():
 			my_window(this, "koku-yuv4mpeg-3D", 640, 480, true),
 			my_rect(&my_window),
-			my_eye_left(&my_window),
-			my_eye_right(&my_window),
+			my_frame(&my_window),
 			my_shader(&my_window),
-			my_eye_sampler_left("eye_left"),
-			my_eye_sampler_right("eye_right"),
-			run(true)
+			my_frame_sampler("frame"),
+			run(true),
+			frame_w(-1),
+			frame_h(-1),
+			frame_data(0),
+			frame_data_unpacked(0)
 		{
 			const float vertex[]=
 			{
@@ -51,17 +59,25 @@ class window: public koku::opengl::windowCallback
 								   "void main()\n"
 								   "{\n"
 								   "	pos_fg = (pos+vec2(1.0,1.0))/2.0;\n"
+								   "	pos_fg.y = 1.0-pos_fg.y;\n"
 								   "	gl_Position = vec4(pos, 0, 1);\n"
 								   "}\n");
 
 			my_shader.uploadFragment("#version 400\n"
 									 "layout(location = 0) out vec4 FragColor;\n"
 									 "in vec2 pos_fg;\n"
-									 "uniform sampler2D eye_left;\n"
-									 "uniform sampler2D eye_right;\n"
+									 "uniform sampler2D frame;\n"
 									 "void main()\n"
 									 "{\n"
-									 "	FragColor = texture2D(eye_left, pos_fg);\n"
+									 "	FragColor = texture2D(frame, pos_fg);\n"
+									 //YUV TO RGB
+									 "	float y = FragColor.r;\n"
+									 "	float u = FragColor.g - 0.5;\n"
+									 "	float v = FragColor.b - 0.5;\n"
+									 "	float r = y + 1.402 * v;\n"
+									 "	float g = y - 0.344 * u - 0.714 * v;\n"
+									 "	float b = y + 1.772 * u;\n"
+									 "	FragColor = vec4(r,g,b,1);\n"
 									 "}\n");
 			my_shader.compile();
 
@@ -76,22 +92,72 @@ class window: public koku::opengl::windowCallback
 				image[(x+y*16)*3+2] = ((x+y)/2)*(255/16);
 			}
 
-			my_eye_left.upload((unsigned char*)image, 16, 16, 3);
-			my_eye_right.upload((unsigned char*)image, 16, 16, 3);
+			my_frame.upload(image, 16, 16, 3);
 
 			delete image;
 		}
 
 		bool update()
 		{
+			//get data from yuv4mpeg
+			if (frame_w == -1)
+			{
+				//setup
+				char c;
+				cin >> c;
+				if (c != 'W')
+				{
+					cout << "YUV4MPEG2 File is wrong ! - Waiting for W" << endl;
+					return false;
+				}
+				cin >> frame_w;
+
+				cin >> c;
+				if (c != 'H')
+				{
+					cout << "YUV4MPEG2 File is wrong ! - Waiting for H" << endl;
+					return false;
+				}
+				cin >> frame_h;
+
+				frame_data = new unsigned char[frame_w*frame_h*3/2];
+				frame_data_unpacked = new unsigned char[frame_w*frame_h*3];
+			}
+			//wait for FRAME
+			string data;
+			while(data != "FRAME")
+			{
+				cin >> data;
+				if (!cin.good())
+				{
+					return false;
+				}
+			}
+			//read FRAME
+			char ignore;
+			cin.read(&ignore, 1); //first byte is nothing..
+			cin.read(reinterpret_cast<char*>(frame_data), frame_w*frame_h*3/2);
+			//convert packed to unpacked
+			const unsigned char* data_y = frame_data;
+			const unsigned char* data_u = frame_data+frame_w*frame_h;
+			const unsigned char* data_v = frame_data+frame_w*frame_h+frame_w/2*frame_h/2;
+			for(int y = 0; y < frame_h; ++y)
+			for(int x = 0; x < frame_w; ++x)
+			{
+				frame_data_unpacked[(x+y*frame_w)*3+0] = data_y[x+y*frame_w]; //Y
+				frame_data_unpacked[(x+y*frame_w)*3+1] = data_u[x/2+y/2*frame_w/2]; //U
+				frame_data_unpacked[(x+y*frame_w)*3+2] = data_v[x/2+y/2*frame_w/2]; //V
+			}
+			//upload unpacked
+			my_frame.upload(frame_data_unpacked, frame_w, frame_h, 3);
+			//update window
 			my_window.update();
 			//render stuff and co
 			my_window.begin();
 				glClearColor(1,1,1,1);
 				glClear(GL_COLOR_BUFFER_BIT);
 				my_shader.begin();
-					my_shader.set(&my_eye_sampler_left,  &my_eye_left);
-					my_shader.set(&my_eye_sampler_right, &my_eye_right);
+					my_shader.set(&my_frame_sampler,  &my_frame);
 					my_rect.render(3, 6);
 				my_shader.end();
 				my_window.flip();
@@ -100,11 +166,36 @@ class window: public koku::opengl::windowCallback
 		}
 };
 
-int main()
+int main(int argc, const char** argv)
 {
-	//does nothing for now
-	window my_window;
+	string data;
+	cin >> data;
 
+	if (data != "YUV4MPEG2")
+	{
+		cout << "You are using this programm wrong ! Use it this way:" << endl;
+		cout << endl;
+		cout << "MODE_INPUT can be:" << endl;
+		cout << "	NONE" << endl;
+		cout << "	LEFT_RIGHT" << endl;
+		cout << "	TOP_BOTTOM" << endl;
+		cout << "MODE_OUTPUT can be:" << endl;
+		cout << "	NONE" << endl;
+		cout << "	LEFT_RIGHT" << endl;
+		cout << "	TOP_BOTTOM" << endl;
+		cout << "	COL" << endl;
+		cout << "	ROW" << endl;
+
+		cout << endl << "Sample:" << endl;
+		cout << " mplayer -vo yuv4mpeg:file=>(" << argv[0] << " LEFT_RIGHT ROW) \"very nice 3D video.wmv\"" << endl;
+		cout << "	-> LEFT_RIGHT input  => left-side left-eye and right-side right-eye" << endl;
+		cout << "	-> ROW        output => first row left-eye and second row right-eye" << endl;
+
+		return 1;
+	}
+
+
+	window my_window;
 	while(my_window.update())
 	{
 		//wait for data
